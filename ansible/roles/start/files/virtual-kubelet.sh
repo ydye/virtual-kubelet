ab10ad4b9f04bc9fca67d544d5924e73718cfbe9
YundongYe <YundongYe@outlook.com>
2020-05-21 14:40:16 +0800

    permission fix

--- ansible/roles/start/files/virtual-kubelet.sh
+++ ansible/roles/start/files/virtual-kubelet.sh
@@ -4,5 +4,6 @@ set -e
 
 nodename_prefix=`hostname`
 
+chmod a+x /usr/bin/virtual-kubelet
 /usr/bin/virtual-kubelet --provider mock --provider-config /etc/virtual-kubelet/vk-config.yml --enable-node-lease=true --node-number 150 --nodename $nodename_prefix --kubeconfig /etc/virtual-kubelet/kubeconfig
 
